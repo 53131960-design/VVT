14de90cd08673ef84e0883ccbc7a5665b3b1aec8
53131960-design <53131960@qq.com>
2025-11-04 08:02:13 +0800

    Delete Hypervisor directory

--- Hypervisor/AntiCheat/AntiCheat.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <ntifs.h>
-
-void MonitoringStart(PVOID Address, size_t Size);
--- Hypervisor/Intel/VMX.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <ntifs.h>
-
-namespace IntelVMX {
-	bool InstallVmx();
-	void UninstallVmx();
-};
-
